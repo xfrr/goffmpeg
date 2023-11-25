876a87c03e49b1ccc3790b8fdd1e904f3e5caadf
xfrr <francisco.romero.1994@gmail.com>
2023-11-25 03:27:15 +0100

    Cleanup pkg

---
+++ pkg/media/stream.go
@@ -0,0 +1,46 @@
+package media
+
+type FileStream struct {
+	Index              int
+	ID                 string      `json:"id"`
+	CodecName          string      `json:"codec_name"`
+	CodecLongName      string      `json:"codec_long_name"`
+	Profile            string      `json:"profile"`
+	CodecType          string      `json:"codec_type"`
+	CodecTimeBase      string      `json:"codec_time_base"`
+	CodecTagString     string      `json:"codec_tag_string"`
+	CodecTag           string      `json:"codec_tag"`
+	Width              int         `json:"width"`
+	Height             int         `json:"height"`
+	CodedWidth         int         `json:"coded_width"`
+	CodedHeight        int         `json:"coded_height"`
+	HasBFrames         int         `json:"has_b_frames"`
+	SampleAspectRatio  string      `json:"sample_aspect_ratio"`
+	DisplayAspectRatio string      `json:"display_aspect_ratio"`
+	PixFmt             string      `json:"pix_fmt"`
+	Level              int         `json:"level"`
+	ChromaLocation     string      `json:"chroma_location"`
+	Refs               int         `json:"refs"`
+	QuarterSample      string      `json:"quarter_sample"`
+	DivxPacked         string      `json:"divx_packed"`
+	RFrameRrate        string      `json:"r_frame_rate"`
+	AvgFrameRate       string      `json:"avg_frame_rate"`
+	TimeBase           string      `json:"time_base"`
+	DurationTs         int         `json:"duration_ts"`
+	Duration           string      `json:"duration"`
+	Disposition        Disposition `json:"disposition"`
+	BitRate            string      `json:"bit_rate"`
+}
+
+type Disposition struct {
+	Default         int `json:"default"`
+	Dub             int `json:"dub"`
+	Original        int `json:"original"`
+	Comment         int `json:"comment"`
+	Lyrics          int `json:"lyrics"`
+	Karaoke         int `json:"karaoke"`
+	Forced          int `json:"forced"`
+	HearingImpaired int `json:"hearing_impaired"`
+	VisualImpaired  int `json:"visual_impaired"`
+	CleanEffects    int `json:"clean_effects"`
+}
